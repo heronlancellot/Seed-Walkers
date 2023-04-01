74b6678de36c321974f1c1c9cbbce5cffbe99816
heronlancellot <heronlancellot@id.uff.br>
2023-04-01 15:03:33 -0300

    Feat: Add Components Site

---
+++ seed/src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styles from '../styles/Header.module.css';
+
+
+const Header = () => {
+  return (
+    <Header className={styles.header__container}>
+      <div className={styles.header__container_metamask}>
+      </div>
+    </Header>
+  );
+};
+
+export default Header;
